8309ac59b7480b556db46b53132cb560698d6426
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-21 15:50:44 +0700

    Get api weather

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("weather app");
+const async_request = require("async-request");
+
+const getWeather = async (location) => {
+  const ACCESS_KEY = "899c062d78c0f921705db195996d3280";
+  const url = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${location}`;
+  try {
+    const res = await async_request(url);
+    const data = JSON.parse(res.body);
+    const weather = {
+      isSuccess: true,
+      region: data.location.region,
+      country: data.location.country,
+      temperature: data.current.temperature,
+      wind_speed: data.current.wind_speed,
+      precip: data.current.precip,
+      cloudcover: data.current.precip,
+    };
+    console.log(weather);
+    return weather;
+  } catch (error) {
+    console.log(error);
+    return {
+      isSuccess: false,
+      error,
+    };
+  }
+};
+
+getWeather("tokyo");
