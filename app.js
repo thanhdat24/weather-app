73922d2bf7e17b143368e33c302b028b2a1e4299
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-21 18:00:27 +0700

    Render variable

--- app.js
+++ app.js
@@ -39,7 +39,14 @@ app.get("/", async (req, res) => {
   const location = params.address;
   const weather = await getWeather(location);
   console.log(weather);
-  res.render("weather");
+  res.render("weather", {
+    region: weather.region,
+    country: weather.country,
+    temperature: weather.temperature,
+    wind_speed: weather.wind_speed,
+    precip: weather.precip,
+    cloudcover: weather.cloudcover,
+  });
 });
 
 app.set("view engine", "hbs");
