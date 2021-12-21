d4218bf92e13874271a2f9a82022cbe1680df6ca
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-12-21 17:54:35 +0700

    Update query params

--- app.js
+++ app.js
@@ -26,15 +26,24 @@ const getWeather = async (location) => {
   }
 };
 
-// getWeather("tokyo");
-
 const express = require("express");
 const app = express();
+const path = require("path");
+
+const pathPublic = path.join(__dirname, "./public");
+app.use(express.static(pathPublic));
 
 // http://localhost:7000/
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  const params = req.query;
+  const location = params.address;
+  const weather = await getWeather(location);
+  console.log(weather);
+  res.render("weather");
 });
+
+app.set("view engine", "hbs");
+
 const port = 7000;
 app.listen(port, () => {
   console.log(`app run on port http://localhost:${port}/`);
